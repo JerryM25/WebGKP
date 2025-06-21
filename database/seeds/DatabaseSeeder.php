b0b6ab8114ce56f03ef13d95f8895692933c03c3
JerryMN25 <72210464@students.ukdw.ac.id>
2025-06-21 15:22:44 +0700

    Akun seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,18 +14,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         akun::create([
-            'username' => 'admin',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('password'),
+            'username' => 'glorykaryaperkasa',
+            'email' => 'glorykaryaperkasa@gmail.com',
+            'password' => Hash::make('Glorykaryaperkasa_53'),
             'id' => '1',
         ]);
 
         // Buat user biasa
-        akun::create([
-            'username' => 'user1',
-            'email' => 'user1@example.com',
-            'password' => Hash::make('password'),
-            'id' => '2',
-        ]);
+        // akun::create([
+        //     'username' => 'user1',
+        //     'email' => 'user1@example.com',
+        //     'password' => Hash::make('password'),
+        //     'id' => '2',
+        // ]);
     }
 }
