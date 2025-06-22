9619526019f4e1b6d7ef613be7d51bf911265552
JerryMN25 <72210464@students.ukdw.ac.id>
2025-06-22 14:22:55 +0700

    Minor Update

--- resources/views/form-edit-news.blade.php
+++ resources/views/form-edit-news.blade.php
@@ -16,8 +16,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
 
-    <link rel="shortcut icon" href="{{ asset('assets/images/logo-removebg.png') }}" type="image/x-icon">
-    <link rel="icon" href="{{ asset('assets/images/logo-removebg.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('assets/images/logo-gkp-1.png') }}" type="image/png">
 
     <!-- Dropzone JS Lib -->
     <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
